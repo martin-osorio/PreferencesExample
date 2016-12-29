a097ee60706ae10130d7b08596f3aa6b0acc6c76
martinosoriolopez <me@martinosorio.com>
2016-12-29 09:57:59 -0500

    Great prefs example

---
+++ app/src/main/java/com/example/martinosorio/preferencesexample/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.martinosorio.preferencesexample;
+
+import android.content.SharedPreferences;
+import android.os.PersistableBundle;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+    private EditText et;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        et = (EditText) findViewById(R.id.ET);
+
+        SharedPreferences settings = getSharedPreferences("File", MODE_PRIVATE);
+        String name = settings.getString("Name", "NO NAME");
+        et.setHint(name);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+        SharedPreferences settings = getSharedPreferences("File", MODE_PRIVATE);
+        SharedPreferences.Editor editor = settings.edit();
+        String name = et.getText().toString();
+        if(name.equals("")){
+            name = et.getHint().toString();
+        }
+        editor.putString("Name", name);
+        editor.commit();
+    }
+}
